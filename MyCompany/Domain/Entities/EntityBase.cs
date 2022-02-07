bc65bc22956128c74ac3902107a8ad61953fbc00
Juli Gabrielyan <juli.gabrielyan.11@gmail.com>
2022-02-07 22:54:16 +0400

    Добавьте файлы проекта.

---
+++ MyCompany/Domain/Entities/EntityBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        protected EntityBase() => DateAdded = DateTime.UtcNow;
+
+        [Required]
+        public Guid Id { get;set; }
+
+        [Display(Name = "Title")]
+        public virtual string Title { get; set; }
+
+        [Display(Name = "Subtitle")]
+        public virtual string Subtitle { get; set; }
+
+        [Display(Name = "Full Description")]
+        public virtual string Text { get; set; }
+
+        [Display(Name = "Title Image")]
+        public virtual string TitleImagePath { get; set; }
+
+        [Display(Name = "SEO MetaTitle")]
+        public string MetaTitle { get; set; }
+
+        [Display(Name = "SEO MetaDescription")]
+        public string MetaDescription { get; set; }
+
+        [Display(Name = "SEO MetaKeywords")]
+        public string MetaKeywords { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime DateAdded { get; set; }
+    }
+}
---
+++ MyCompany/Domain/Entities/TextField.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyCompany.Domain.Entities
+{
+    public class TextField : EntityBase
+    {
+        [Required]
+        public string CodeWord { get; set; }
+
+        [Display(Name = "Title")]
+        public override string Title { get; set; } = "Info Page";
+
+        [Display(Name = "Content")]
+        public override string Text { get; set; } = "Content is filled by admin";
+    }
+}
